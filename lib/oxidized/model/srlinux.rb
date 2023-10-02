c195bfea1bff23209f8abbb1b6a57f87dbdea4ac
Jeroen van Bemmel <jvb127@gmail.com>
2023-10-02 23:12:39 +0000

    Working version, still some control codes in output

--- lib/oxidized/model/srlinux.rb
+++ lib/oxidized/model/srlinux.rb
@@ -9,90 +9,39 @@ class SRLINUX < Oxidized::Model
 
   comment '# '
 
-  prompt /^A:([-\w.@:>*]+\s?[#]\s?)$/
+  # Multiline prompt, control chars filtered below
+  prompt /\s[:]([\w.@_()-:]+[#]\s+)$/
 
-  cmd :all do |cfg, cmdstring|
-    new_cfg = comment "COMMAND: #{cmdstring}\n"
-    cfg.gsub! /# Finished .*/, ''
-    cfg.gsub! /# Generated .*/, ''
-    cfg.delete! "\r"
-    new_cfg << cfg.cut_both
+  # replace all used vt100 control sequences with a single space
+  # expect /\e\[\??\w+(;\w+)*/ do |data, re|
+  expect /\e\[\??\w+/ do |data, re|
+    data.gsub re, ' '
   end
 
-  #
-  # Show the system information.
-  #
-  cmd 'show system information' do |cfg|
-    #
-    # Strip uptime.
-    #
-    cfg.sub! /^System Up Time.*\n/, ''
-    comment cfg
-  end
 
-  #
-  # Show the card state.
-  #
-  cmd 'show card state' do |cfg|
-    comment cfg
-  end
+#  cmd :all do |cfg, cmdstring|
+#    new_cfg = comment "COMMAND: #{cmdstring}\n"
+#    cfg.gsub! /# Finished .*/, ''
+#    cfg.gsub! /# Generated .*/, ''
+#    cfg.delete! "\r"
+#    new_cfg << cfg.cut_both
+#  end
 
   #
   # Show the chassis information.
   #
-  cmd 'show chassis' do |cfg|
+  cmd 'show /platform chassis' do |cfg|
     comment cfg.lines.to_a[0..25].reject { |line| line.match /state|Time|Temperature|Status/ }.join
-  end
-
-  #
-  # Show the boot log.
-  #
-  cmd 'file show bootlog.txt' do |cfg|
-    cfg.gsub! /[\b][\b][\b]/, "\n"
-    comment cfg
-  end
-
-  #
-  # Show the running debug configuration.
-  #
-  cmd 'admin show configuration debug full-context' do |cfg|
-    comment cfg
-  end
-
-  #
-  # Show the saved debug configuration (admin debug-save).
-  #
-  cmd 'file show config.dbg' do |cfg|
-    comment cfg
-  end
-
-  #
-  # Show the running persistent indices.
-  #
-  cmd 'admin show configuration configure | match persistent-indices post-lines 10000' do |cfg|
-    comment cfg
-  end
-
-  #
-  # Show the boot options file.
-  #
-  cmd 'admin show configuration bof full-context' do |cfg|
-    cfg
   end
 
   #
   # Show the running configuration.
   #
-  cmd 'admin show configuration configure full-context' do |cfg|
+  cmd 'info from running' do |cfg|
     cfg
   end
 
-  cfg :telnet do
-    username /^Login: /
-    password /^Password: /
-  end
-
-  cfg :telnet, :ssh do
+  cfg :ssh do
     pre_logout 'quit'
   end
 end
